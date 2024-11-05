5106d99b2df3faddb99f43384402703baa137d18
Rose <rrortegaa@gmail.com>
2024-11-04 20:39:02 -0600

    added dashboard component UI and functionality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CatologyComponent } from '../catology/catology.component';
+import { PurrfectService } from '../purrfect.service';
+import { Catology } from '../catology';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CatologyComponent, NavbarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  purrfectService: PurrfectService = inject(PurrfectService);
+
+  catologyList: Catology[] = [];
+
+  constructor() {
+    this.purrfectService.getAllCats().then((catologyList: Catology[]) => {
+      this.catologyList = catologyList;
+    });
+
+    // this.catologyList = this.purrfectService.getAllCats();
+  }
 }
