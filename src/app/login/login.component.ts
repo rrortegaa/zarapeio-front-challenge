1e25432b95554df32da3bd7e3f531ccf3a93b9b1
Rose <rrortegaa@gmail.com>
2024-11-06 00:51:29 -0600

    improve format of the code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  authService: AuthService = inject(AuthService);
+
+  constructor() {}
+
+  submit() {
+    // console.log('email:', this.loginForm.value.email);
+    this.authService.login(
+      this.loginForm.value.email ?? '',
+      this.loginForm.value.password ?? ''
+    );
+  }
 }
