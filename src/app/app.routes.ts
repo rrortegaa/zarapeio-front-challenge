e6b0bb5887c934c5e2afd5c94b2a3f6375204ba7
Rose <rrortegaa@gmail.com>
2024-11-06 00:54:15 -0600

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login page',
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    title: 'Dashboard page',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile page',
+  },
+];
