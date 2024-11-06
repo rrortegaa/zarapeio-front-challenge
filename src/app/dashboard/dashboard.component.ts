ffe5b11b85feaf1a48f341a7e701638025d5cb3f
Rose <rrortegaa@gmail.com>
2024-11-06 00:57:33 -0600

    update logic and UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,29 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CatologyComponent } from '../catology/catology.component';
 import { PurrfectService } from '../purrfect.service';
 import { Catology } from '../catology';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CatologyComponent, NavbarComponent],
+  imports: [CommonModule, HttpClientModule, CatologyComponent, NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-  // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-
+export class DashboardComponent implements OnInit, OnDestroy {
   purrfectService: PurrfectService = inject(PurrfectService);
-
   catologyList: Catology[] = [];
+  loading: boolean = true;
+  #subscriptions: Subscription = new Subscription();
 
-  constructor() {
-    this.purrfectService.getAllCats().then((catologyList: Catology[]) => {
-      this.catologyList = catologyList;
-    });
+  constructor() {}
 
-    // this.catologyList = this.purrfectService.getAllCats();
+  ngOnInit(): void {
+    const catsSubscription = this.purrfectService
+      .getAllCats()
+      .subscribe((catologyList: Catology[]) => {
+        this.catologyList = catologyList;
+        this.loading = false;
+      });
+
+    this.#subscriptions.add(catsSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.#subscriptions.unsubscribe();
   }
 }
