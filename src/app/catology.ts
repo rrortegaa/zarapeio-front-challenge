ed3c20fbe0d630f212b4175d014b662ff324a243
Rose <rrortegaa@gmail.com>
2024-11-06 04:06:56 -0600

    rename component and services

--- src/app/catology.ts
+++
@@ -1,9 +0,0 @@
-export interface Catology {
-  id: number;
-  name: string;
-  description: string;
-  temperament: string;
-  reference_image_id: string;
-  origin: string;
-  wikipedia_url: string;
-}
